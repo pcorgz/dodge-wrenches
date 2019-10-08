0d7734caab3995c4ff8125034101629960385159
Pablo C. Ortiz G <pablo.orgz@gmail.com>
2019-10-07 22:33:02 -0500

    game over animation and restart button

--- DodgeWrenches/Assets/Scripts/GameMechanics/GameManager.cs
+++ DodgeWrenches/Assets/Scripts/GameMechanics/GameManager.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static bool isGameOver;
+
+    [SerializeField]
+    private GameObject gameOverScreen = null;
 
     private void Start()
     {
@@ -14,6 +18,18 @@ public class GameManager : MonoBehaviour
         if (PlayerStats.Health == 0)
         {
             isGameOver = true;
+            gameOverScreen.SetActive(true);
         }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Menu()
+    {
+        //SceneManager.LoadScene(0);
+        Debug.Log("Go To Menu");
     }
 }
